c7adadf72224ab254f680e42e3d5338a745fa66c
Danish <danishjpiet@gmail.com>
2023-10-10 17:02:04 +0530

    vercel config chagne

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/api', (req, res) => {
+app.get('/', (req, res) => {
   res.status(200).json({ message: 'api working' });
 });
 
